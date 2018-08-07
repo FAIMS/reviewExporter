73e2d7a42360d6b8d76bb8f2dc72d6cfe40f2a3e
Brian Ballsun-Stanton <brian@fedarch.org>
2018-08-07 14:55:47 +1000

    making install sript more paranoid

--- install.sh
+++ install.sh
@@ -1,13 +1,13 @@
 . /etc/lsb-release
-if [ "$DISTRIB_CODENAME" == "trusty" ]; then
-	sudo add-apt-repository ppa:jonathonf/texlive-2016 -y
-fi
-if [ "$DISTRIB_CODENAME" == "xenial" ]; then
-	sudo add-apt-repository ppa:jonathonf/texlive-2017 -y
-fi
+# if [ "$DISTRIB_CODENAME" == "trusty" ]; then
+# 	sudo add-apt-repository ppa:jonathonf/texlive-2016 -y
+# fi
+# if [ "$DISTRIB_CODENAME" == "xenial" ]; then
+# 	sudo add-apt-repository ppa:jonathonf/texlive-2017 -y
+# fi
 sudo apt update
 
-sudo apt-get install spatialite-bin libimage-exiftool-perl imagemagick scantailor libtiff-tools pdftk tesseract-ocr exactimage poppler-utils pandoc parallel context python python-regex python-pip python-pil -y
+sudo apt-get install spatialite-bin libimage-exiftool-perl imagemagick scantailor libtiff-tools pdftk tesseract-ocr exactimage poppler-utils pandoc parallel python-dev build-essential python python-regex python-pip python-pil -y
 sudo pip install -U dateparser
 
 #todo add dateparsing capabilities
@@ -21,7 +21,7 @@ sudo touch /root/.parallel/will-cite
 sudo mkdir /usr/local/context
 sudo chown ubuntu /usr/local/context
 cd /usr/local/context
-wget http://minimals.contextgarden.net/setup/first-setup.sh
+wget -r http://minimals.contextgarden.net/setup/first-setup.sh
 sh first-setup.sh --modules=all
 
 
